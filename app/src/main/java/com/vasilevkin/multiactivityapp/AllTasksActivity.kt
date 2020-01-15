95d6c9f2f0af3b0003ab48e7613e8492054d97e0
Sergey Vasilevkin <svasilev@mera.ru>
2020-01-15 22:17:28 +0300

    Add recyclerView to All tasks activity.

--- app/src/main/java/com/vasilevkin/multiactivityapp/AllTasksActivity.kt
+++ app/src/main/java/com/vasilevkin/multiactivityapp/AllTasksActivity.kt
@@ -1,12 +1,48 @@
 package com.vasilevkin.multiactivityapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class AllTasksActivity : AppCompatActivity() {
+
+class AllTasksActivity : AppCompatActivity(), AllTasksRecyclerViewAdapter.ItemClickListener {
+
+    internal lateinit var adapter: AllTasksRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_tasks)
+
+        // data to populate the RecyclerView with
+        val tasks = ArrayList<Task>()
+        tasks.add(Task("Horse", "new1", false))
+        tasks.add(Task("Cow", "new2", false))
+        tasks.add(Task("Pig", "desc3", false))
+
+        // set up the RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.all_tasks_recyclerview)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.setLayoutManager(layoutManager)
+        adapter = AllTasksRecyclerViewAdapter(this, tasks)
+        adapter.setClickListener(this)
+        recyclerView.setAdapter(adapter)
+
+        val dividerItemDecoration = DividerItemDecoration(
+            recyclerView.context,
+            layoutManager.getOrientation()
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
+    }
+
+    override fun onItemClick(view: View, position: Int) {
+        Toast.makeText(
+            this,
+            "You clicked " + adapter.getItem(position) + " on row number " + position,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
---
+++ app/src/main/java/com/vasilevkin/multiactivityapp/AllTasksRecyclerViewAdapter.kt
@@ -0,0 +1,73 @@
+package com.vasilevkin.multiactivityapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AllTasksRecyclerViewAdapter// data is passed into the constructor
+internal constructor(context: Context, private val data: List<Task>) :
+    RecyclerView.Adapter<AllTasksRecyclerViewAdapter.ViewHolder>() {
+    private val inflater: LayoutInflater
+    private var clickListener: ItemClickListener? = null
+
+    init {
+        this.inflater = LayoutInflater.from(context)
+    }
+
+    // inflates the row layout from xml when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.task_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // binds the data to the TextView in each row
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val taskTitle = data[position].title
+        val taskDescription = data[position].description
+
+        holder.titleTextView.text = taskTitle
+        holder.descriptionTextView.text = taskDescription
+    }
+
+    // total number of rows
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        internal var titleTextView: TextView
+        internal var descriptionTextView: TextView
+
+        init {
+            titleTextView = itemView.findViewById(R.id.task_list_title)
+            descriptionTextView = itemView.findViewById(R.id.task_list_description)
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View) {
+            if (clickListener != null) clickListener!!.onItemClick(view, adapterPosition)
+        }
+    }
+
+    // convenience method for getting data at click position
+    internal fun getItem(id: Int): Task {
+        return data[id]
+    }
+
+    // allows clicks events to be caught
+    internal fun setClickListener(itemClickListener: ItemClickListener) {
+        this.clickListener = itemClickListener
+    }
+
+    // parent activity will implement this method to respond to click events
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+}
